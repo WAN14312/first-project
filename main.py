976c93160227b1c5af1c7c9c73a865aff60aee04
WAN14312 <wanamirhaziq134@gmail.com>
2025-07-13 14:25:33 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+x = 1
+print(x)
+print(type(x))
+
+y = "Hello World"
+print(y)
+print(type(y))
+
+f = 3.0
+print(f)
+print(type(f))
+
+k = None
+print(k)
+print(type(k))
+
+j = False
+print(j)
+print(type(j))
+
+for i in [1,2,3,4,5]:
+    print(i+20)
